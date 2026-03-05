b0ba15f38cbc21adddfd346446f4c6e2eed4492e
Jean-Daniel Poulain <jeandpoulain@gmail.com>
2026-03-05 11:24:43 +0100

    mise en place de la fonction usine de sprite

--- main.lua
+++ main.lua
@@ -6,8 +6,23 @@ end
 -- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
 io.stdout:setvbuf("no")
 
+listSprite = {}
+
+--creation fonction usine de sprite
+function creatSprite(pType, pX, pY)
+    local sprite = {}
+    sprite.type = pType
+    sprite.x = pX
+    sprite.y = pY
+    table.insert(listSprite, sprite)
+    return sprite
+end
+
+
 function love.load()
-  
+    hero1 = creatSprite("hero", 10, 10)
+    print("hero est positioner a : "..hero1.x)
+
 end
 
 function love.update(dt)
