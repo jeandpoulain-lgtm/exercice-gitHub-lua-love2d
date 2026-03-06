f755fcc256e0a227eebccef2174449d91e0ceb13
Jean-Daniel Poulain <jeandpoulain@gmail.com>
2026-03-06 07:14:14 +0100

    mise en place du hero et factorisation

--- main.lua
+++ main.lua
@@ -6,26 +6,35 @@ end
 -- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
 io.stdout:setvbuf("no")
 
-sprite = require "spriteManager"
-
-
+require "spriteManager"
+WindowWidth = love.graphics.getWidth()
+WindowHeight = love.graphics.getHeight()
 
 function love.load()
-    hero1 = creatSprite("hero", 10, 50)
-    
-    hero2 = creatSprite("enemie1", 100, 100)
-    
+    hero1 = creatSprite("hero",  WindowWidth/2 - 50 , WindowHeight - 60, 50, 50)
+    hero1.speed = 400
 end
 
 function love.update(dt)
-    
 
-     
+    if love.keyboard.isDown("right") then
+        hero1.x = hero1.x + hero1.speed * dt
+        if hero1.x > WindowWidth - hero1.width then
+            hero1.x = WindowWidth - hero1.width
+        end
+    end
+
+    if love.keyboard.isDown("left") then
+        hero1.x = hero1.x - hero1.speed * dt
+        if hero1.x < 0 then
+            hero1.x = 0
+        end
+    end
+         
 end
 
 function love.draw()
-    
-    love.graphics.rectangle("fill", hero1.x, hero1.y, 50, 50)
+    love.graphics.rectangle("fill", hero1.x, hero1.y, hero1.width, hero1.height)
 end
 
 function love.keypressed(key)
--- spriteManager.lua
+++ spriteManager.lua
@@ -1,11 +1,19 @@
 listSprite = {}
 
 --creation fonction usine de sprite
-function creatSprite(pType, pX, pY)
+function creatSprite(pType, pX, pY, pW, pH)
     local sprite = {}
     sprite.type = pType
     sprite.x = pX
     sprite.y = pY
+    sprite.width = pW
+    sprite.height = pH
+    sprite.rotation = 0
+    sprite.scaleX = 1
+    sprite.scaleY = 1
+    sprite.speed = 0
+    sprite.image = nil
+    sprite.radius = 0
     sprite.del = false
     table.insert(listSprite, sprite)
     return sprite
